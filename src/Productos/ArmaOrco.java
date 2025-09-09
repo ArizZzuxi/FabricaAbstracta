9cd71f1c3a1b9379dc7645caf0cff267c378fd5b
Dizast3r <jm2006900@gmail.com>
2025-09-09 18:27:43 -0500

    Creando claseees :(

---
+++ src/Productos/ArmaOrco.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Productos;
+
+/**
+ *
+ * @author User
+ */
+public class ArmaOrco implements Arma{
+    private String descripcion;
+
+    public ArmaOrco() {
+    }
+
+    public ArmaOrco(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String darDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
---
+++ src/Productos/ArmaduraEnano.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Productos;
+
+/**
+ *
+ * @author User
+ */
+public class ArmaduraEnano implements Armadura{
+
+    private String descripcion;
+
+    public ArmaduraEnano() {
+    }
+
+    public ArmaduraEnano(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String darDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
